905c155b140f0208ace00178d74bc1507374a926
Jennifer Gryczkowski <accounts@rycz.com>
2017-07-24 02:03:08 +0000

    Added code comments for Stripe

--- saasapp/app/controllers/application_controller.rb
+++ saasapp/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  # Whitelist the following form fields so that we can process them, if coming form
+  # a Devise sign up form.
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
   
   protected
     def configure_permitted_parameters
